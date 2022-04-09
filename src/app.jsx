40cc1337a67bebebff4767e82ba61c09ebc2a937
Nuttapol Benz Phomthon <sevipsagis@gmail.com>
2022-04-09 17:24:00 +0700

    add: lazy load

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,38 @@
 import Router from 'preact-router'
-import Home from './containers/home'
-import About from './containers/about'
-import Experience from './containers/experience'
-import Project from './containers/project'
-import NotFound from './containers/404'
+import AsyncRoute from 'preact-async-route'
 
-export default function App(props) {
+export default function App() {
     return (
         <div class="min-w-full min-h-screen flex flex-wrap justify-center items-center">
             <Router>
-                <Home path="/" />
-                <About path="/about" />
-                <Experience path="/experience" />
-                <Project path="/project/:projectId" />
-                <NotFound default />
+                <AsyncRoute
+                    path="/"
+                    getComponent={() =>
+                        import('./containers/home').then((module) => module.default)
+                    }
+                />
+                <AsyncRoute
+                    path="/about"
+                    getComponent={() =>
+                        import('./containers/about').then((module) => module.default)
+                    }
+                />
+                <AsyncRoute
+                    path="/experience"
+                    getComponent={() =>
+                        import('./containers/experience').then((module) => module.default)
+                    }
+                />
+                <AsyncRoute
+                    path="/project/:projectId"
+                    getComponent={() =>
+                        import('./containers/project').then((module) => module.default)
+                    }
+                />
+                <AsyncRoute
+                    default
+                    getComponent={() => import('./containers/404').then((module) => module.default)}
+                />
             </Router>
         </div>
     )
