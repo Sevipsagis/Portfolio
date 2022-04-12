b7f58ff8a1875d5954abea61f50d5fb358a5cb9e
Nuttapol Benz Phomthon <sevipsagis@gmail.com>
2022-04-12 23:42:52 +0700

    update: color style and add profile picture on about section

--- src/containers/about.jsx
+++ src/containers/about.jsx
@@ -1,11 +1,25 @@
 import { route } from 'preact-router'
-import profilePicture from '../assets/images/my_profile.jpg'
+import profilePicture from '../assets/images/profile.png'
 
 export default function About() {
     return (
         <section className="min-w-10/10 min-h-10/10 flex flex-wrap justify-center items-center snap-start">
-            <div class="w-full min-h-10/10 bg-base-300 flex flex-wrap items-center justify-center md:justify-start">
-                <div class="md:m-24"></div>
+            <div class="w-full min-h-10/10 bg-base-300 p-8 md:p-12 lg:p-16 flex flex-wrap lg:items-center lg:justify-start">
+                <div class="relative w-12/12 h-fit lg:w-6/12">
+                    <img
+                        src={profilePicture}
+                        class="w-11/12 md:w-10/12 lg:w-12/12 mr-auto ml-auto"
+                    />
+                    <h1
+                        class="absolute uppercase font-medium text-accent-focus leading-tight text-3xl md:text-7xl lg:text-5xl"
+                        style={{
+                            bottom: '15%',
+                            right: '10%',
+                        }}
+                    >
+                        About Me
+                    </h1>
+                </div>
             </div>
         </section>
     )
